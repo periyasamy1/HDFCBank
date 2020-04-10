1def75081a3b1798a1ea28247af8539e54696756
Dev1 <periyasamy@periyasamy-PC>
2020-04-10 15:27:01 +0530

    dev1 modified test1.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,10 @@
 package com.nt.test;
 
-public class Test {
+import com.nt.beans.Utility;
 
+public class Test {
+	public static void main(String args[]) {
+		Utility o = new Utility();
+		System.out.println(o.getsysdate());
+	}
 }
